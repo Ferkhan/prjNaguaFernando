350e6158a7a996be74ba0d1958270b482442a162
Ferkhan-Lab <ferkhan@epn.edu.ec>
2023-06-28 14:18:01 -0500

    crear login

--- src/App.java
+++ src/App.java
@@ -1,7 +1,62 @@
 import java.util.Scanner;
 
 public class App {
+    protected static Scanner fnScanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
+        if (fnLogin()) {
+            System.out.println("todo bien");
+        } else {
+            System.out.println("Gracias");
+        }
         
+
+
+    }
+
+    private static boolean fnLogin() {
+        String[] fnListaUsuarios = {"fernando.nagua@epn.edu.ec", "profe"};
+        String[] fnListaClaves = {"1724721301", "1234"};
+
+        int fnIntentosMax = 3;
+        int fnIntentosRestantes = fnIntentosMax;
+        boolean fnAcceso = false;
+        String fnUsuarioAutenticado = "";
+
+        do {
+            System.out.println("......................");
+            System.out.print("+ usuario:  ");
+            String fnUsuario = fnScanner.nextLine();
+            
+            System.out.print("+ clave:    ");
+            String fnClave = fnScanner.nextLine();
+            System.out.println("......................");
+
+
+            for (int i = 0; i < fnListaUsuarios.length; i++) {
+                if (fnUsuario.equals(fnListaUsuarios[i]) && fnClave.equals(fnListaClaves[i])) {
+                    fnAcceso = true;
+                    fnUsuarioAutenticado = fnUsuario.toUpperCase();
+                    break;
+                }
+            }
+
+            if (!fnAcceso) {
+                fnIntentosRestantes--;
+                System.out.println("* Nro de intentos: " + fnIntentosRestantes);
+
+                if (fnIntentosRestantes == 0) {
+                    System.out.println("¡Lo sentimos, tu usuario y clave son incorrectos...!");
+                    break;
+                }
+            }
+
+        } while (!fnAcceso);
+        
+        if (fnAcceso) {
+            System.out.println(":: Bienvenido " + fnUsuarioAutenticado);
+        }
+
+        return fnAcceso;
+        // fnScanner.close();
     }
 }
