34dd0419439d4b8f1a253dc6be7c3976feba6e86
Ferkhan-Lab <ferkhan@epn.edu.ec>
2023-06-28 14:03:42 -0500

    iniciar proyecto

---
+++ src/FNUtility/FNUtileria.java
@@ -0,0 +1,14 @@
+package FNUtility;
+
+public class FNUtileria {
+
+
+    public static void fnDatosPersonales() {
+        System.out.println("CEDULA:  1724721301");
+        System.out.println("CORREO:  FERNANDO.NAGUA@EPN.EDU.EC");
+        System.out.println("NOMBRE:  fernando alexander nagua uyaguari");
+
+    }
+
+    
+}
